e6e2dc965f911387f2ab1743ae9b8177309e2b2a
Kristen Kulha and Mitchell Long <mitchell@>
2017-09-18 11:50:53 -0700

    add due date attribute to Task class

--- lib/task.rb
+++ lib/task.rb
@@ -1,9 +1,10 @@
 class Task
-  attr_reader(:description, :list_id)
+  attr_reader(:description, :list_id, :due_date)
 
   def initialize(attributes)
     @description = attributes.fetch(:description)
     @list_id = attributes.fetch(:list_id)
+    @due_date = attributes.fetch(:due_date)
   end
 
   def self.all
@@ -12,16 +13,21 @@ class Task
     returned_tasks.each() do |task|
       description = task.fetch("description")
       list_id = task.fetch("list_id").to_i() # The information comes out of the database as a string.
-      tasks.push(Task.new({:description => description, :list_id => list_id}))
+      due_date = task.fetch("due_date")
+      tasks.push(Task.new({:description => description, :list_id => list_id, :due_date => due_date}))
     end
     tasks
   end
 
   def save
-    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
+    DB.exec("INSERT INTO tasks (description, list_id, due_date) VALUES ('#{@description}', #{@list_id},'#{@due_date}') ;")
   end
 
   def ==(another_task)
     self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id()))
   end
+
+  # def self.sort
+  #   DB.exec("SELECT * FROM tasks ORDER BY due_date ASC;")
+  # end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -11,30 +11,41 @@ describe(Task) do
 
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2017-11-21'})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
   end
 
+  # describe("#sort") do
+  #   it("sorts tasks by their due date in descending order") do
+  #     task1 = Task.new({:description => "go to dentist", :list_id => 1, :due_date => '2017-11-21' })
+  #     task2 = Task.new({:description => "get a job", :list_id => 2, :due_date => '2017-11-03'})
+  #     task1.save()
+  #     task2.save()
+  #     Task.sort();
+  #     expect(Task.all()).to(eq([task2, task1]))
+  #   end
+  # end
+
   describe("#description") do
     it("lets you read the description out") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2017-11-21'})
       expect(test_task.description()).to(eq("learn SQL"))
     end
   end
 
   describe("#list_id") do
     it("lets you read the list ID out") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2017-11-21'})
       expect(test_task.list_id()).to(eq(1))
     end
   end
 
   describe("#==") do
     it("is the same task if it has the same description and list ID") do
-      task1 = Task.new({:description => "learn SQL", :list_id => 1})
-      task2 = Task.new({:description => "learn SQL", :list_id => 1})
+      task1 = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2017-11-21'})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1,:due_date => '2017-11-21'})
       expect(task1).to(eq(task2))
     end
   end
