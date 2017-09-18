4ad8a493f33bd69c1317516b097c6e304278ab26
Kristen Kulha and Mitchell Long <mitchell@>
2017-09-18 16:36:32 -0700

    add form to add tasks to lists

--- app.rb
+++ app.rb
@@ -2,7 +2,42 @@ require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("./lib/task")
+require("./lib/list")
 require("pg")
 
 
 DB = PG.connect({:dbname => "to_do"})
+
+get("/") do
+  erb(:index)
+end
+
+get('/lists') do
+  @lists = List.all()
+  erb(:lists)
+end
+
+get("/lists/new") do
+  erb(:list_form)
+end
+
+get("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list)
+end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:success)
+end
+
+post("/tasks") do
+  description = params.fetch("description")
+  list_id = params.fetch("list_id").to_i()
+  @list = List.find(list_id)
+  @task = Task.new({:description => description, :list_id => list_id, :due_date => '2017-11-23'})
+  @task.save()
+  erb(:success)
+end
--- lib/list.rb
+++ lib/list.rb
@@ -25,4 +25,25 @@ class List
   def ==(another_list)
     self.name().==(another_list.name()).&(self.id().==(another_list.id()))
   end
+
+  def self.find(id)
+    found_list = nil
+    List.all().each() do |list|
+      if list.id().==(id)
+        found_list = list
+      end
+    end
+    found_list
+  end
+  def tasks
+    list_tasks = []
+    tasks = DB.exec("SELECT * FROM tasks WHERE list_id = #{self.id()};")
+    tasks.each do |task|
+      description = task.fetch("description")
+      list_id = task.fetch("list_id").to_i()
+      due_date = task.fetch("due_date")
+      list_tasks.push(Task.new({:description => description, :list_id => list_id, :due_date => due_date}))
+    end
+    list_tasks
+  end
 end
