f2f585d8e6fbbd6ad2c66cf7cdc6e2b6fc88da6a
Gennaro Anzalone and Mitchell Long <gennaro@>
2017-09-20 10:25:10 -0700

    add update and delete features

--- app.rb
+++ app.rb
@@ -41,3 +41,22 @@ post("/tasks") do
   @task.save()
   erb(:success)
 end
+
+get("/lists/:id/edit") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list_edit)
+end
+
+patch("/lists/:id") do
+  name = params.fetch("name")
+  @list = List.find(params.fetch("id").to_i())
+  @list.update({:name => name})
+  erb(:list)
+end
+
+delete("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  @list.delete()
+  @lists = List.all()
+  erb(:index)
+end
